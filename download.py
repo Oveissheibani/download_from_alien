55abab12130427cf54996c291aee61fe58082830
Oveis Sheibani <54555081+Oveissheibani@users.noreply.github.com>
2023-06-28 14:04:19 -0500

    Add files via upload

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-local_dir = "/path/to/your/local/directory/"
+local_dir = "/Users/oveis/Desktop/child1_extra/"
 directory = "/alice/data/2016/LHC16q/000265500/pass2_CENT_wSDD/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_1/"
 
 # Fetch all AnalysisResults.root file paths
@@ -16,3 +16,25 @@ for run_dir in run_directories:
     run_id = os.path.basename(run_dir)
     print("Downloading "+target_path)
     subprocess.run(["alien.py","cp","alien:"+target_path,"file:"+local_dir+"AnalysisResults_"+run_id+".root"])
+    
+    
+    
+#/alice/data/2016/LHC16q/000265424/pass2_CENT_wSDD/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_1
+    
+#/alice/data/2016/LHC16q/000265378/pass2_CENT_wSDD/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_1
+
+
+#/alice/data/2016/LHC16q/000265344/pass2_CENT_wSDD/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_1
+
+
+#/alice/data/2016/LHC16q/000265343/pass2_CENT_wSDD/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_1
+
+
+#/alice/data/2016/LHC16q/000265500/pass2_FAST/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_2
+
+#/alice/data/2016/LHC16q/000265378/pass2_FAST/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_2
+
+
+#/alice/data/2016/LHC16q/000265344/pass2_FAST/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_2
+
+#/alice/data/2016/LHC16q/000265343/pass2_FAST/AOD244/PWGHF/D2H_pPb/1488_20230412-0508_child_2
